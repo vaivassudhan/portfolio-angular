8694004ad38089d920edb0e400b1335509e391f5
Senthil Sivaraman <vaivassaravanan1@gmail.com>
2021-12-21 10:05:44 +0530

    skill update

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -5,14 +5,6 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
-  // animations: [
-  //   trigger('fade', [
-  //     transition('void => *', [ 
-  //       style({ opacity: 0 }),
-  //       animate(2500, style({ opacity: 1 }))
-  //     ])
-  //   ])
-  // ]
 })
 export class ProfileComponent implements OnInit {
 
