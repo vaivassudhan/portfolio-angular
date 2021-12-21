81b151456e2e26298a0cc51972510052dc86a0b0
Senthil Sivaraman <vaivassaravanan1@gmail.com>
2021-12-21 17:31:27 +0530

    ui

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -10,32 +10,32 @@ export class ContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    // var scene = new THREE.Scene();
+    // var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-    var renderer = new THREE.WebGLRenderer();
-    var container = document.getElementById('container')!;
-    renderer.setSize( window.innerWidth, window.innerHeight/2 );
-    container.appendChild( renderer.domElement );
+    // var renderer = new THREE.WebGLRenderer();
+    // var container = document.getElementById('container')!;
+    // renderer.setSize( window.innerWidth, window.innerHeight/2 );
+    // container.appendChild( renderer.domElement );
     
 
-    var geometry = new THREE.SphereGeometry( 1,64,64 );
-    var material = new THREE.MeshStandardMaterial();
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    // var geometry = new THREE.SphereGeometry( 1,64,64 );
+    // var material = new THREE.MeshStandardMaterial();
+    // var cube = new THREE.Mesh( geometry, material );
+    // scene.add( cube );
 
-    camera.position.z = 5;
+    // camera.position.z = 5;
 
-    var animate = function () {
-      requestAnimationFrame( animate );
+    // var animate = function () {
+    //   requestAnimationFrame( animate );
 
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+    //   cube.rotation.x += 0.01;
+    //   cube.rotation.y += 0.01;
 
-      renderer.render( scene, camera );
-    };
+    //   renderer.render( scene, camera );
+    // };
 
-    animate();
+    // animate();
   }
 
 }
