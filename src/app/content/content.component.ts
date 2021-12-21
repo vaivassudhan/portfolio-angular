8499834e401377b284477679bd1e577a895d2263
Senthil Sivaraman <vaivassaravanan1@gmail.com>
2021-12-21 11:09:02 +0530

    animation

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as THREE from 'three';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
@@ -10,6 +10,32 @@ export class ContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+    var renderer = new THREE.WebGLRenderer();
+    var container = document.getElementById('container')!;
+    // renderer.setSize( container.clientWidth, container.clientHeight );
+    container.appendChild( renderer.domElement );
+    
+
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5;
+
+    var animate = function () {
+      requestAnimationFrame( animate );
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render( scene, camera );
+    };
+
+    animate();
   }
 
 }
