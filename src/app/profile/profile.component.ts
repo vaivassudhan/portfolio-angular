cb5ef170302f656c6b451146e1798f8c828fd7b6
Senthil Sivaraman <vaivassaravanan1@gmail.com>
2021-12-21 10:40:23 +0530

    profile component skills

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  public mySkills=[
+    {"name":"Java","percentage":"90%"},
+    {"name":"Python","percentage": "85%"},
+    {"name":"JavaScript","percentage": "80%"},
+    {"name":"MongoDB, MySQL","percentage": "80%"},
+    {"name":"Angular","percentage": "75%"},
+  ]
+  public knowledge=["Cloud Deployment","API","Networking (Core)","Internet of Things (IoT)"]
   constructor() { }
 
   ngOnInit(): void {
