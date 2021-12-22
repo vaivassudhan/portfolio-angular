986d072e9ad82defd66e459fda491986ad844a95
Senthil Sivaraman <vaivassaravanan1@gmail.com>
2021-12-22 11:56:52 +0530

    some ui updates

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -12,10 +12,11 @@ export class ProfileComponent implements OnInit {
     {"name":"Java","percentage":"90%"},
     {"name":"Python","percentage": "85%"},
     {"name":"JavaScript","percentage": "80%"},
+    {"name":"MEAN Stack","percentage": "75%"},
     {"name":"MongoDB, MySQL","percentage": "80%"},
-    {"name":"Angular","percentage": "75%"},
+    {"name":"UI & UX","percentage": "75%"},
   ]
-  public knowledge=["Cloud Deployment","API","Networking (Core)","Internet of Things (IoT)"]
+  public knowledge=["Cloud ","API","Networking (Core)","Internet of Things (IoT)","Blockchain"]
   constructor() { }
 
   ngOnInit(): void {
