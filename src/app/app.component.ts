a96011d2523d3258a03b8ce202ac70bb9d4cee07
Senthil Sivaraman <vaivassaravanan1@gmail.com>
2021-12-22 11:32:04 +0530

    cleaning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,108 +9,106 @@ import * as dat from 'dat.gui'
 })
 export class AppComponent implements OnInit {
   title = 'portfolio';
+  public sidebarShow: boolean = false;
   ngOnInit(): void {
     
-    // Debug
-    const gui = new dat.GUI()
+    // // Debug
+    // const gui = new dat.GUI()
 
-    // Canvas
-    const canvas = document.querySelector('canvas.webgl')
+    // // Canvas
+    // const canvas = document.querySelector('canvas.webgl')!
 
-    // Scene
-    const scene = new THREE.Scene()
+    // // Scene
+    // const scene = new THREE.Scene()
 
-    // Objects
-    const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+    // // Objects
+    // const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
 
-    // Materials
+    // // Materials
 
-    const material = new THREE.MeshBasicMaterial()
-    material.color = new THREE.Color(0xff0000)
+    // const material = new THREE.MeshBasicMaterial()
+    // material.color = new THREE.Color(0xff0000)
 
-    // Mesh
-    const sphere = new THREE.Mesh(geometry,material)
-    scene.add(sphere)
+    // // Mesh
+    // const sphere = new THREE.Mesh(geometry,material)
+    // scene.add(sphere)
 
-    // Lights
+    // // Lights
 
-    const pointLight = new THREE.PointLight(0xffffff, 0.1)
-    pointLight.position.x = 2
-    pointLight.position.y = 3
-    pointLight.position.z = 4
-    scene.add(pointLight)
+    // const pointLight = new THREE.PointLight(0xffffff, 0.1)
+    // pointLight.position.x = 2
+    // pointLight.position.y = 3
+    // pointLight.position.z = 4
+    // scene.add(pointLight)
 
-    /**
-     * Sizes
-     */
-    const sizes = {
-        width: window.innerWidth,
-        height: window.innerHeight
-    }
+    // /**
+    //  * Sizes
+    //  */
+    // const sizes = {
+    //     width: window.innerWidth,
+    //     height: window.innerHeight
+    // }
 
-    window.addEventListener('resize', () =>
-    {
-        // Update sizes
-        sizes.width = window.innerWidth
-        sizes.height = window.innerHeight
+    // window.addEventListener('resize', () =>
+    // {
+    //     // Update sizes
+    //     sizes.width = window.innerWidth
+    //     sizes.height = window.innerHeight
 
-        // Update camera
-        camera.aspect = sizes.width / sizes.height
-        camera.updateProjectionMatrix()
+    //     // Update camera
+    //     camera.aspect = sizes.width / sizes.height
+    //     camera.updateProjectionMatrix()
 
-        // Update renderer
-        renderer.setSize(sizes.width, sizes.height)
-        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-    })
+    //     // Update renderer
+    //     renderer.setSize(sizes.width, sizes.height)
+    //     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    // })
 
-    /**
-     * Camera
-     */
-    // Base camera
-    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-    camera.position.x = 0
-    camera.position.y = 0
-    camera.position.z = 2
-    scene.add(camera)
+    // /**
+    //  * Camera
+    //  */
+    // // Base camera
+    // const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+    // camera.position.x = 0
+    // camera.position.y = 0
+    // camera.position.z = 2
+    // scene.add(camera)
 
-    // Controls
-    // const controls = new OrbitControls(camera, canvas)
-    // controls.enableDamping = true
+    // // Controls
 
-    /**
-     * Renderer
-     */
-    const renderer = new THREE.WebGLRenderer({
-        canvas: canvas!
-    })
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    // /**
+    //  * Renderer
+    //  */
+    // const renderer = new THREE.WebGLRenderer({
+    //     canvas: canvas!
+    // })
+    // renderer.setSize(sizes.width, sizes.height)
+    // renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-    /**
-     * Animate
-     */
+    // /**
+    //  * Animate
+    //  */
 
-    const clock = new THREE.Clock()
+    // const clock = new THREE.Clock()
+    // const tick = () =>
+    // {
 
-    const tick = () =>
-    {
+    //     const elapsedTime = clock.getElapsedTime()
 
-        const elapsedTime = clock.getElapsedTime()
+    //     // Update objects
+    //     sphere.rotation.y = .5 * elapsedTime
 
-        // Update objects
-        sphere.rotation.y = .5 * elapsedTime
+    //     // Update Orbital Controls
+    //     // controls.update()
 
-        // Update Orbital Controls
-        // controls.update()
+    //     // Render
+    //     renderer.render(scene, camera)
 
-        // Render
-        renderer.render(scene, camera)
+    //     // Call tick again on the next frame
+    //     window.requestAnimationFrame(tick)
+    // }
 
-        // Call tick again on the next frame
-        window.requestAnimationFrame(tick)
-    }
-
-    tick()
+    // tick()
   }
   
 }
