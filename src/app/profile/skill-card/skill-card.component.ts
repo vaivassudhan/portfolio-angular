d9e124c9013ce384768bd1a9b6e9f11783ae396c
Senthil Sivaraman <vaivassaravanan1@gmail.com>
2021-12-23 07:59:28 +0530

    skill set change

--- src/app/profile/skill-card/skill-card.component.ts
+++ src/app/profile/skill-card/skill-card.component.ts
@@ -12,11 +12,11 @@ export class SkillCardComponent implements OnInit {
     {"name":"Java","percentage":"90%"},
     {"name":"Python","percentage": "85%"},
     {"name":"JavaScript","percentage": "80%"},
-    {"name":"MEAN Stack","percentage": "75%"},
-    {"name":"MongoDB, MySQL","percentage": "80%"},
+    {"name":"MEAN Stack","percentage": "80%"},
+    {"name":"MongoDB, MySQL","percentage": "75%"},
     {"name":"UI & UX","percentage": "75%"},
   ]
-  public knowledge=["Cloud ","API","Networking (Core)","Internet of Things (IoT)","Blockchain"]
+  public knowledge=["Cloud ","API","Networking","Internet of Things","Blockchain"]
   ngOnInit(): void {
   }
 
