93048bdab025232d98d6d008558d0e9651d861c0
Senthil Sivaraman <vaivassaravanan1@gmail.com>
2021-12-23 09:00:33 +0530

    project cards

--- src/app/content/projects/projects.component.ts
+++ src/app/content/projects/projects.component.ts
@@ -8,7 +8,33 @@ import { Component, OnInit } from '@angular/core';
 export class ProjectsComponent implements OnInit {
 
   constructor() { }
-
+  public Projects = [
+    {
+      "title":"R&D Portal",
+      "description":"A Centralized portal to manage and record research projects, Publications and fundings of professors in Sona college.",
+      "images":[
+        {"class":"html-logo","src":"../../../assets/img/html.png"},
+        {"class":"css-logo","src":"../../../assets/img/css.png"},
+        {"class":"angular-logo","src":"../../../assets/img/angular.png"},
+        {"class":"node-logo","src":"../../../assets/img/node1.png"},
+        {"class":"mongodb-logo","src":"../../../assets/img/mongodb.png"},
+        {"class":"aws-logo","src":"../../../assets/img/aws.png"}
+      ],
+      "link":"researchportal.sonatech.ac.in"
+    },
+    {
+      "title":"Sahaayata",
+      "description":"Web portal and mobile app to simplify the greivance redressal of employees of the Southern Railway Salem division.",
+      "images":[
+        {"class":"html-logo","src":"../../../assets/img/html.png"},
+        {"class":"css-logo","src":"../../../assets/img/css.png"},
+        {"class":"angular-logo","src":"../../../assets/img/angular.png"},
+        {"class":"node-logo","src":"../../../assets/img/node1.png"},
+        {"class":"mongodb-logo","src":"../../../assets/img/mongodb.png"},
+      ],
+      "link":"researchportal.sonatech.ac.in"
+    }
+  ]
   ngOnInit(): void {
   }
 
