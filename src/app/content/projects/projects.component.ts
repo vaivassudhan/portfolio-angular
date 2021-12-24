a69f90b69fefa059d8b3be4e3bacc1dd9a3806e6
Senthil Sivaraman <vaivassaravanan1@gmail.com>
2021-12-24 10:37:36 +0530

    all set up done

--- src/app/content/projects/projects.component.ts
+++ src/app/content/projects/projects.component.ts
@@ -11,7 +11,7 @@ export class ProjectsComponent implements OnInit {
   public Projects = [
     {
       "title":"R&D Portal",
-      "description":"A Centralized portal to manage and record research projects, Publications and fundings of professors in Sona college.",
+      "description":"A centralised R&D web portal for Sona college. Currently has around 200 users and deployed on AWS EC2.",
       "images":[
         {"class":"html-logo","src":"../../../assets/img/html.png"},
         {"class":"css-logo","src":"../../../assets/img/css.png"},
@@ -24,7 +24,7 @@ export class ProjectsComponent implements OnInit {
     },
     {
       "title":"Sahaayata",
-      "description":"Web portal and mobile app to simplify the greivance redressal of employees of the Southern Railway Salem division.",
+      "description":"Built a web portal and mobile app to simplify the grievance redressal of employees of the Southern Railway.",
       "images":[
         {"class":"html-logo","src":"../../../assets/img/html.png"},
         {"class":"css-logo","src":"../../../assets/img/css.png"},
@@ -36,92 +36,53 @@ export class ProjectsComponent implements OnInit {
     },
     {
       "title":"Ideathon - Covicare",
-      "description":"A mobile app to support and aid the self-quarantining patients by helping analyze healthy home quarantine routine.",
+      "description":"App supported by Johns Hopkins University to guide people isolating at home after testing Covid-19 positive",
       "images":[
-        {"class":"html-logo","src":"../../../assets/img/html.png"},
-        {"class":"css-logo","src":"../../../assets/img/css.png"},
-        {"class":"angular-logo","src":"../../../assets/img/angular.png"},
-        {"class":"node-logo","src":"../../../assets/img/node1.png"},
-        {"class":"mongodb-logo","src":"../../../assets/img/mongodb.png"},
-      ],
-      "link":"researchportal.sonatech.ac.in"
-    },
-    {
-      "title":"R&D Portal",
-      "description":"A Centralized portal to manage and record research projects, Publications and fundings of professors in Sona college.",
-      "images":[
-        {"class":"html-logo","src":"../../../assets/img/html.png"},
-        {"class":"css-logo","src":"../../../assets/img/css.png"},
-        {"class":"angular-logo","src":"../../../assets/img/angular.png"},
-        {"class":"node-logo","src":"../../../assets/img/node1.png"},
-        {"class":"mongodb-logo","src":"../../../assets/img/mongodb.png"},
-        {"class":"aws-logo","src":"../../../assets/img/aws.png"}
-      ],
-      "link":"researchportal.sonatech.ac.in"
-    },
-    {
-      "title":"Sahaayata",
-      "description":"Web portal and mobile app to simplify the greivance redressal of employees of the Southern Railway Salem division.",
-      "images":[
-        {"class":"html-logo","src":"../../../assets/img/html.png"},
-        {"class":"css-logo","src":"../../../assets/img/css.png"},
-        {"class":"angular-logo","src":"../../../assets/img/angular.png"},
-        {"class":"node-logo","src":"../../../assets/img/node1.png"},
-        {"class":"mongodb-logo","src":"../../../assets/img/mongodb.png"},
-      ],
-      "link":"researchportal.sonatech.ac.in"
-    },
-    {
-      "title":"Ideathon - Covicare",
-      "description":"A mobile app to support and aid the self-quarantining patients by helping analyze healthy home quarantine routine.",
-      "images":[
-        {"class":"html-logo","src":"../../../assets/img/html.png"},
-        {"class":"css-logo","src":"../../../assets/img/css.png"},
-        {"class":"angular-logo","src":"../../../assets/img/angular.png"},
-        {"class":"node-logo","src":"../../../assets/img/node1.png"},
-        {"class":"mongodb-logo","src":"../../../assets/img/mongodb.png"},
-      ],
-      "link":"researchportal.sonatech.ac.in"
-    },
-    {
-      "title":"R&D Portal",
-      "description":"A Centralized portal to manage and record research projects, Publications and fundings of professors in Sona college.",
-      "images":[
-        {"class":"html-logo","src":"../../../assets/img/html.png"},
-        {"class":"css-logo","src":"../../../assets/img/css.png"},
-        {"class":"angular-logo","src":"../../../assets/img/angular.png"},
-        {"class":"node-logo","src":"../../../assets/img/node1.png"},
-        {"class":"mongodb-logo","src":"../../../assets/img/mongodb.png"},
-        {"class":"aws-logo","src":"../../../assets/img/aws.png"}
-      ],
-      "link":"researchportal.sonatech.ac.in"
-    },
-    {
-      "title":"Sahaayata",
-      "description":"Web portal and mobile app to simplify the greivance redressal of employees of the Southern Railway Salem division.",
-      "images":[
-        {"class":"html-logo","src":"../../../assets/img/html.png"},
-        {"class":"css-logo","src":"../../../assets/img/css.png"},
-        {"class":"angular-logo","src":"../../../assets/img/angular.png"},
-        {"class":"node-logo","src":"../../../assets/img/node1.png"},
-        {"class":"mongodb-logo","src":"../../../assets/img/mongodb.png"},
-      ],
-      "link":"researchportal.sonatech.ac.in"
-    },
-    {
-      "title":"Ideathon - Covicare",
-      "description":"A mobile app to support and aid the self-quarantining patients by helping analyze healthy home quarantine routine.",
-      "images":[
-        {"class":"html-logo","src":"../../../assets/img/html.png"},
-        {"class":"css-logo","src":"../../../assets/img/css.png"},
-        {"class":"angular-logo","src":"../../../assets/img/angular.png"},
-        {"class":"node-logo","src":"../../../assets/img/node1.png"},
-        {"class":"mongodb-logo","src":"../../../assets/img/mongodb.png"},
+        {"class":"react-logo","src":"../../../assets/img/react.png"},
+        {"class":"css-logo","src":"../../../assets/img/api.png"},
+
       ],
       "link":"researchportal.sonatech.ac.in"
     },
 
-
+    {
+      "title":"Car Monitoring System",
+      "description":"Designed a Raspberry Pi-powered OBD-II car device that allows data from a car to be displayed on a mobile app.",
+      "images":[
+        {"class":"react-logo","src":"../../../assets/img/react.png"},
+        {"class":"aws-logo","src":"../../../assets/img/firebase.png"},
+        {"class":"aws-logo","src":"../../../assets/img/RPI.png"},
+        // {"class":"node-logo","src":"../../../assets/img/node1.png"},
+        // {"class":"mongodb-logo","src":"../../../assets/img/mongodb.png"},
+        // {"class":"aws-logo","src":"../../../assets/img/aws.png"}
+      ],
+      "link":"researchportal.sonatech.ac.in"
+    },
+    {
+      "title":"Sahaayata",
+      "description":"Web portal and mobile app to simplify the greivance redressal of employees of the Southern Railway Salem division.",
+      "images":[
+        {"class":"html-logo","src":"../../../assets/img/html.png"},
+        {"class":"css-logo","src":"../../../assets/img/css.png"},
+        {"class":"angular-logo","src":"../../../assets/img/angular.png"},
+        {"class":"node-logo","src":"../../../assets/img/node1.png"},
+        {"class":"mongodb-logo","src":"../../../assets/img/mongodb.png"},
+      ],
+      "link":"researchportal.sonatech.ac.in"
+    },
+    {
+      "title":"Ideathon - Covicare",
+      "description":"A mobile app to support and aid the self-quarantining patients by helping analyze healthy home quarantine routine.",
+      "images":[
+        {"class":"html-logo","src":"../../../assets/img/html.png"},
+        {"class":"css-logo","src":"../../../assets/img/css.png"},
+        {"class":"angular-logo","src":"../../../assets/img/angular.png"},
+        {"class":"node-logo","src":"../../../assets/img/node1.png"},
+        {"class":"mongodb-logo","src":"../../../assets/img/mongodb.png"},
+      ],
+      "link":"researchportal.sonatech.ac.in"
+    },
+  
   ]
   ngOnInit(): void {
   }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,6 +14,21 @@ export class HomeComponent implements OnInit {
     var height = (document.getElementById('profile'))!.offsetHeight;
     console.log(height)
     // document.getElementById('content')!.style.height=height+100+"px";
+
+  }
+  
+  sidebartog(){
+    this.sidebarShow = !this.sidebarShow;
+    if(this.sidebarShow){
+      document.getElementById('content')!.style.zIndex = "-1";
+    }
+    else{
+      document.getElementById('content')!.style.zIndex = "1";
+    }
+  }
+  exit(){
+    this.sidebarShow = false;
+    document.getElementById('content')!.style.zIndex = "1";
   }
 
 }
