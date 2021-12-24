d76f418a5f97c6eca945127fe1b32141d25a0d97
Senthil Sivaraman <vaivassaravanan1@gmail.com>
2021-12-24 09:32:28 +0530

    nav tab

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -6,16 +6,18 @@ import * as THREE from 'three';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  toRotate: any;
-  el: any;
-  loopNum!: number;
-  period!: number;
-  txt!: string;
-  isDeleting!: boolean;
-
   constructor() { }
-
+  public navlist =["Projects","Certificates","Youtube"];
+  public navselected = [true,false,false];
+  
   ngOnInit(): void {
   }
-
+  change(i: number){
+    // make navselected array all value false
+    for(let j=0;j<this.navselected.length;j++){
+      this.navselected[j]=false;
+    }
+    this.navselected[i]=true;
+    
+  }
 }
